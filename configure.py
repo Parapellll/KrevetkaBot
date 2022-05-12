c2bce8e2454a94b997cbf83c9b9fbd41d48d3f17
Krevetka <101887538+Parapellll@users.noreply.github.com>
2022-05-13 02:36:49 +0300

    Update configure.py

--- configure.py
+++ configure.py
@@ -1,6 +1,6 @@
 config = {
-	'name': 'KrevetkaRanBot', #Имя бота
-	'token': '5315554774:AAETzGjwEaVS_20pBFyohJhThP950U7sq_c', #Токен бота
-	'tokenqiwi': '6d4e9a870bb16a3e5a8cef2929a62248', #Токен киви
-	'phoneqiwi': '+79217038628' #Номер(телефон) киви
+	'name': '', #Имя бота
+	'token': '', #Токен бота
+	'tokenqiwi': '', #Токен киви
+	'phoneqiwi': '' #Номер(телефон) киви
 }
